9eb4d645fdbe6572991f38f160a46d7da5238e05
Thomas Pot <thomas@open2bizz.nl>
2025-07-08 19:31:58 +0200

    [NEW] Fields, filters

--- mgmtsystem_info_security_manual_nen7510/models/document_page.py
+++ mgmtsystem_info_security_manual_nen7510/models/document_page.py
@@ -1,6 +1,6 @@
 #    Copyright (C) 2025 Open2bizz BV www.open2bizz.nl
 
-from odoo import fields, models, _
+from odoo import fields, models, api, _
 from markupsafe import Markup
 
 class DocumentPage(models.Model):
