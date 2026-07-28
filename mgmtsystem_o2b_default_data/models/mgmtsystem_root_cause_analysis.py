5bd2cf5d10ef6747603e8ca158ed9ab6c6efc7f4
Thomas Pot <thomas@open2bizz.nl>
2026-07-28 13:01:59 +0200

    [NEW] Default data templates for Management

--- mgmtsystem_o2b_default_data/models/mgmtsystem_root_cause_analysis.py
+++ mgmtsystem_o2b_default_data/models/mgmtsystem_root_cause_analysis.py
@@ -123,3 +123,59 @@ class MgmtsystemRootCauseAnalysis(models.Model):
             update_data = self.default_incident_identification_factors.get_update_default_data(self.incident_identification_factors)
             if update_data['update']:
                 self.incident_identification_factors = update_data['data']
+
+    # Extend and create functions for default data
+
+    def action_create_default_data(self):
+        self.ensure_one()
+        # Set context
+        # Use this in the view:
+        # context="{'field_name': 'description', 'default_data_field': 'default_description'}"
+        field_name = self.env.context.get('field_name', 'none')
+        default_data_field = self.env.context.get('default_data_field', 'none')
+
+        if field_name == "none":
+            raise ValueError(_("Field name is required! Missing in context"))
+        if default_data_field == "none":
+            raise ValueError(_("Default data field is required! Missing in context"))
+
+        if self[default_data_field]:
+            raise UserError(_("There is already a default linked"))
+
+        def_data_model = self.env['default.data']
+        target_model = self.env['ir.model'].search([('model', '=', self._name)], limit=1)
+        field_id = self.env['ir.model.fields'].search([('model', '=', 'project.task'), ('name', '=', field_name)])
+        value = self[field_name]
+
+        default_data_value = def_data_model.action_create_default_data(target_model, field_id, value)
+        self.write({default_data_field: default_data_value.id})
+
+    def extend_default_data_field(self):
+        self.ensure_one()
+        # Set context
+        # Use this in the view: context="{'field_name', 'destination_field_name'}"
+        field_name = self.env.context.get('field_name', 'none')
+        if field_name == "none":
+            raise ValueError(_("Field name is required! Missing in context"))
+
+        model_obj = self.env['ir.model'].search([('model', '=', self._name)], limit=1)
+        field = self.env['ir.model.fields'].search([('model_id', '=', model_obj.id), ('name', '=', field_name)])
+
+        view = self.env.ref('default_data.extend_default_data_form')
+        context = {
+            'default_model_id': model_obj.id,
+            'default_field_id': field.id,
+            'default_res_id': self.id,
+            'default_position': 'before',
+        }
+        return {
+            "name": "Add text",
+            "type": "ir.actions.act_window",
+            "res_model": "extend.default.data",
+            "view_mode": "form",
+            "views": [
+                [view.id, "form"]
+            ],
+            "target": "new",
+            "context": context
+        }
